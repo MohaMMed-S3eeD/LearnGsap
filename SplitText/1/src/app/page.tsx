4a51a008e3ff6862cdd2ce70430237b78ffe6db9
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-26 19:26:30 +0300

    Remove .hintrc configuration file as it is no longer needed for project setup

---
+++ SplitText/1/src/app/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger, ScrollSmoother);
+
+export default function Home() {
+  useGSAP(() => {
+    // let smoother = ScrollSmoother.create({
+    //   smooth: 1,
+    //   effects: true,
+
+    // });
+
+    let split = SplitText.create("h1", {
+      type: "words",
+      wordsClass: "word",
+    });
+
+    gsap.from(split.words, {
+      opacity: 0.3,
+      // x: 100,
+      delay: 1,
+      stagger: 1,
+      ease: "power2.inOut",
+      duration: 0.1,
+      scrollTrigger: {
+        trigger: "#page",
+        start: "top top",
+        end: "+=300%",
+        scrub: true,
+        // markers: true,
+        pin: true,
+      },
+    });
+  }, []);
+
+  return (
+    <main>
+      <div
+        id="page"
+        className="h-[100dvh] w-screen flex items-center justify-center clip-path-polygon-100"
+      >
+        <h1 className=" text-white text-xl font-light  max-w-[50%]">
+          Modern web development requires a deep understanding of advanced
+          technologies and cutting-edge tools. By utilizing libraries like GSAP
+          and React, we can create stunning interactive experiences and
+          captivating visual effects. Smooth animations and seamless transitions
+          help make user interfaces more engaging and intuitive to use.
+          Continuous development and learning are the keys to success in this
+          field.
+        </h1>
+      </div>
+      <div className="h-[100dvh] w-screen bg-black flex items-center justify-center">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quos.
+        </p>
+      </div>
+    </main>
+  );
+}
